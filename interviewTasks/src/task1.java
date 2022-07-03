cbd063cea248a3b3f229c29464815faf376415df
1srailov <91071916+1srailov@users.noreply.github.com>
2022-07-03 15:42:59 +0500

    ended task1 and task2

--- interviewTasks/src/task1.java
+++ interviewTasks/src/task1.java
@@ -1,2 +1,57 @@
+import java.util.Scanner;
+
 public class task1 {
+    public static Scanner  scanner = new Scanner(System.in);
+
+    public static Integer findDifference(int[][] nums){
+        for(int i = 0; i < 7; i++)
+            for(int j = 0; j < 7; j++)
+                if(nums[i][j] == 1){
+                    int a = i < 3 ? 3 - i : i - 3;
+                    int b = j < 3 ? 3 - j : j - 3;
+                    return a + b;
+                }
+        return 0;
+    }
+
+    public static Integer numberValidation(Integer number){
+        if(number <= 7)
+            return number - 1;
+
+        while(number > 7) {
+            System.out.println("please insert number less than seven: ");
+            number = scanner.nextInt();
+        }
+
+        return number - 1;
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("0 0 0 0 0 0 0\n" +
+                "0 0 0 0 0 0 0\n" +
+                "0 0 0 0 0 0 0\n" +
+                "0 0 0 0 0 0 0\n" +
+                "0 0 0 0 0 0 0\n" +
+                "0 0 0 0 0 0 0\n" +
+                "0 0 0 0 0 0 0\n");
+
+        System.out.println("insert line: ");
+        int a = numberValidation(scanner.nextInt());
+        System.out.println("insert row: ");
+        int b = numberValidation(scanner.nextInt());
+
+        int[][] place = new int[7][7];
+
+        for(int i = 0; i < 7; i++){
+            for(int j = 0; j < 7; j++){
+                if(i == a && j == b)
+                    place[i][j] = 1;
+                else
+                    place[i][j] = 0;
+            }
+        }
+
+        System.out.println(findDifference(place));
+    }
 }
--- interviewTasks/src/task2.java
+++ interviewTasks/src/task2.java
@@ -1,2 +1,54 @@
+import java.util.Scanner;
+
 public class task2 {
+
+    public static Scanner scanner = new Scanner(System.in);
+    static long checkPrime(int n){
+        if(n <= 2){
+            return 0L;
+        }
+        boolean checker = true;
+        long result = 2L;
+        int m;
+        for(int i = 3; i <= n; i++){
+            m = i / 2;
+            for (int j = 2; j <= m; j++) {
+                if (i % j == 0) {
+                    checker = false;
+                    break;
+                }
+            }
+            if (checker) {
+                result = result * i;
+            }
+            checker = true;
+        }
+        return result;
+    }
+
+    public static Integer numberValidation(Integer number){
+        if(number <= 1018)
+            return number;
+
+        while(number > 1018) {
+            System.out.println("please insert number less than 1018: ");
+            number = scanner.nextInt();
+        }
+
+        return number;
+    }
+
+    public static void main(String[] args) {
+        int count = 0;
+        System.out.println("insert number >> ");
+        String num = String.valueOf(checkPrime(numberValidation(scanner.nextInt())));
+        for(int i = num.length() - 1; i != -1; i--){
+            if(num.charAt(i) == '0'){
+                count++;
+            }
+            else break;
+        }
+        System.out.println(count);
+
+    }
 }
